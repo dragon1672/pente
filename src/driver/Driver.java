83c8023375bcea5448d85452b25146decd39b672
Anthony Corbin <dragon1672@gmail.com>
2016-12-30 02:52:07 -0800

    Adding driver test

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -1,7 +1,18 @@
 package driver;
 
+import pente.Board;
+import pente.BoardImpl;
+import pente.Color;
+import utils.IntVector2D;
+
 public class Driver {
     public static void main(String ... args) {
-        System.out.println("Hello World :D");
+        Board board = new BoardImpl(19,19);
+        board.placePiece(IntVector2D.create(9,9), Color.BLACK);
+        board.placePiece(IntVector2D.create(9,10), Color.WHITE);
+        board.placePiece(IntVector2D.create(9,11), Color.WHITE);
+        System.out.println(board.stringify());
+        board.placePiece(IntVector2D.create(9,12), Color.BLACK);
+        System.out.println(board.stringify());
     }
 }
