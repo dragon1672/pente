90188db57907f012865ecb28a1654130f7d3988a
Anthony Corbin <dragon1672@gmail.com>
2016-12-30 15:22:12 -0800

    Adding some streams to readonly board

--- src/pente/ReadOnlyBoard.java
+++ src/pente/ReadOnlyBoard.java
@@ -3,10 +3,54 @@ package pente;
 import utils.IntVector2D;
 import utils.MyStringUtils;
 
+import java.util.stream.Stream;
+
 public interface ReadOnlyBoard {
+    /**
+     * Get Board's width
+     * @return width of board
+     */
     int getWidth();
+
+    /**
+     * Get Board's height
+     * @return height of board
+     */
     int getHeight();
+
+    /**
+     * Get the color of the given position in the board.
+     * An Empty board should return Color.EMPTY for all valid positions
+     * @param pos position to check
+     * @return color at given position
+     * @throws IllegalArgumentException if position is not within board dimensions
+     */
     Color getColor(IntVector2D pos);
+
+    /**
+     * Returns a stream of all valid positions (positions not moves) in the current board
+     * @return a stream of locations
+     */
+    default Stream<IntVector2D> getAllBoardPositions() {
+        return IntVector2D.getRange(0,getWidth(),0,getHeight());
+    }
+
+    /**
+     * Restricts the provided positions to the provided color.
+     * @param ofColor color to restrict by
+     * @return a stream of locations
+     */
+    default Stream<IntVector2D> getPieceLocations(Color ofColor) {
+        return getAllBoardPositions().filter(pos -> getColor(pos) == ofColor);
+    }
+
+    /**
+     * Gets all the piece locations currently on the board. Should not return locations that have Color.EMPTY
+     * @return a stream of locations
+     */
+    default Stream<IntVector2D> getPieceLocations() {
+        return getAllBoardPositions().filter(pos -> getColor(pos) != Color.EMPTY);
+    }
 
     default String stringify() {
         StringBuilder sb = new StringBuilder((getWidth()+3) * (getHeight()+2));
