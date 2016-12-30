7f6cf2f3f19c172ac425bc6f3b6af0f8d2318e1a
Anthony Corbin <dragon1672@gmail.com>
2016-12-30 02:50:56 -0800

    Upgrading stringify to add a border around the box

--- src/pente/ReadOnlyBoard.java
+++ src/pente/ReadOnlyBoard.java
@@ -1,6 +1,7 @@
 package pente;
 
 import utils.IntVector2D;
+import utils.MyStringUtils;
 
 public interface ReadOnlyBoard {
     int getWidth();
@@ -8,16 +9,24 @@ public interface ReadOnlyBoard {
     Color getColor(IntVector2D pos);
 
     default String stringify() {
-        StringBuilder sb = new StringBuilder((getWidth()+1) * getHeight());
+        StringBuilder sb = new StringBuilder((getWidth()+3) * (getHeight()+2));
+
+        sb.append(MyStringUtils.repeatChar('-',getWidth()+2,'+'));
+        sb.append('\n');
+
         for (int y = 0; y < getHeight(); y++) {
+            sb.append('|');
             for (int x = 0; x < getWidth(); x++) {
                 IntVector2D pos = IntVector2D.create(x,y);
                 Color color = getColor(pos);
                 char ch = color.toChar();
                 sb.append(ch);
             }
+            sb.append('|');
             sb.append('\n');
         }
+
+        sb.append(MyStringUtils.repeatChar('-',getWidth()+2,'+'));
         return sb.toString();
     }
 }
