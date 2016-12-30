1a2e2f4b7af0c7d47701bbe8625e8f8dac065618
Anthony Corbin <dragon1672@gmail.com>
2016-12-30 11:28:43 -0800

    Scope changes

--- src/pente/Action.java
+++ src/pente/Action.java
@@ -4,16 +4,17 @@ import utils.IntVector2D;
 import utils.Tuple;
 import utils.Tuple.Tuple3;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Action {
     // {position, color change from, color change to}
-    final List<Tuple3<IntVector2D, Color, Color>> changes;
+    public final Collection<Tuple3<IntVector2D, Color, Color>> changes;
 
     public Action(List<Tuple3<IntVector2D, Color, Color>> changes) {
-        this.changes = Collections.unmodifiableList(changes);
+        this.changes = Collections.unmodifiableCollection(changes);
     }
 
     public Action reverse() {
