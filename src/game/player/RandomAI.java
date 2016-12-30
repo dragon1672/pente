975a486c5126ea4ccc6c46ad7f0a87f866cbdc23
Anthony Corbin <dragon1672@gmail.com>
2016-12-30 15:25:14 -0800

    Cleaning up code

--- src/game/player/RandomAI.java
+++ src/game/player/RandomAI.java
@@ -7,24 +7,10 @@ import utils.IntVector2D;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class RandomAI implements Player {
 
     private static final Random rand = new Random();
-
-    private static Stream<IntVector2D> getAllPositions(ReadOnlyBoard board) {
-        return IntStream.range(0,board.getWidth()).mapToObj(x ->
-                IntStream.range(0,board.getHeight()).mapToObj(y->
-                        IntVector2D.create(x,y)))
-                .flatMap(stream->stream);
-    }
-
-    private static Stream<IntVector2D> getValidMoves(ReadOnlyBoard board) {
-        return getAllPositions(board)
-                .filter(pos -> !board.getColor(pos).isPlayer);
-    }
 
     private static <T> T getRandomInList(List<T> list) {
         return list.get(rand.nextInt(list.size()));
@@ -32,6 +18,9 @@ public class RandomAI implements Player {
 
     @Override
     public IntVector2D placePiece(ReadOnlyBoard board, Color pieceToPlace) {
-        return getRandomInList(getValidMoves(board).collect(Collectors.toList()));
+        List<IntVector2D> possibleMoves = board.getAllBoardPositions()
+                .filter(pos -> board.getColor(pos) == Color.EMPTY)
+                .collect(Collectors.toList());
+        return getRandomInList(possibleMoves);
     }
 }
